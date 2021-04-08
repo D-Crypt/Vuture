8dd7a57c6ef46958621c86a158099144af024ddd
D-Crypt <danielvaughan54@gmail.com>
2021-04-08 16:20:53 +0100

    Add functions for task 3A

--- script.js
+++ script.js
@@ -62,3 +62,50 @@ console.log(isPalindrome('I have some cheese'));
 console.log(isPalindrome("God saved Eva's dog"));
 console.log(isPalindrome("saippuakivikauppias"));
 console.log(isPalindrome("race CAR"));
+
+/* Task 3
+
+Part A)
+
+Create a function which counts the number of occurrences of words from a "censored words list" in a text.
+
+Example:
+
+Input:
+
+{"dog", "cat", "large"} and "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse."
+Output:
+cat: 1, dog: 2, large: 1, total: 4 */
+
+function checkNumberOfOccurrencesCensored(list, text) {
+    const splitText = text.split(" ");
+    const map = convertListToMap(list);
+
+    // TODO: Remove punctuation, make lowercase, retain spaces
+
+    for (let i = 0; i < splitText.length; i++) {
+        const word = splitText[i];
+
+        if (map.has(word)) {
+            map.set(word, (map.get(word)) + 1);
+        }
+    }
+
+    return map;
+}
+
+function convertListToMap(list) {
+    const map = new Map();
+
+    for (let i = 0; i < list.length; i++) {
+        const word = list[i];
+        map.set(word, 0);
+    }
+
+    return map;
+}
+
+const list = ["dog", "cat", "large"];
+const text = "I have a cat named Meow and a dog name Woof. I love the dog a lot. He is larger than a small horse."
+
+console.log(checkNumberOfOccurrencesCensored(list, text));
